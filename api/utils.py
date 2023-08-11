58ebb6bc52ec0b1c5a7d64b1cc19a3c7431a7fa8
Kate Olazabal <kate@bitmaker.la>
2023-08-10 21:52:24 -0500

    data

--- api/utils.py
+++ api/utils.py
@@ -1,11 +1,25 @@
 import pandas as pd
 import os
+
 current_file = os.path.abspath(os.path.dirname(__file__))
 
 
-def get_by_year(
-    year: int,
-):
-    df_2021 = pd.read_csv(os.path.join(current_file, 'data/2021.csv'))
-    results = df_2021.head(5).to_dict('records')
-    return results
+def get_by_type(primary_types):
+    results = []
+
+    for primary_type in primary_types:
+        filename = os.path.join(current_file, f"data/{primary_type}.csv")
+        df = pd.read_csv(filename)
+
+        results.extend(df.to_dict('records'))
+
+    df = pd.DataFrame(results)
+
+    df['Datefull'] = pd.to_datetime(df['Datefull'])
+
+    df = df.sort_values(by='Datefull')
+
+    # Agrupar por fecha y sumar los totales
+    df['TOTALES'] = df.groupby('Date')['Count'].transform('sum')
+
+    return df.head(10000)
